ff32ada12b93fba17f9c767708518868fbb57767
ABIODUN ABBEY AINA <olamilekansunday445@gmail.com>
2025-05-11 12:01:01 +0000

    first commit

---
+++ listy/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1>Listy</h1>
+      <p>Welcome to Listy, your go-to app for managing lists!</p>
+    </div>
+  );
+}
+
+export default App;
